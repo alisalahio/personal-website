54cfb85a874370ea182c7639488e928bcbe8a544
alisalahio <ali@salah.io>
2020-05-15 13:17:14 +0200

    add weird page

---
+++ src/pages/ali.salah.io/ali.salah.io/ali.salah.io/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './shit.css'
+
+const DeepShit = () => (
+  <div class="holder">
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+	<div class="circle"></div>
+</div>
+)
+
+export default DeepShit
